edcd2e55e66d88b8be7e29951ebd4062691174f3
alierfanianomidvar <ali.erfanianomidvar@gmail.com>
2024-11-18 14:31:10 +0100

    Add: gradio for the ui of chatbot

---
+++ app_gradio.py
@@ -0,0 +1,29 @@
+import gradio as gr
+from services.answering.answering_service import AnsweringService
+
+
+# Define the chatbot logic
+def chatbot_response(user_input):
+    chatbot_service = AnsweringService()  # Initialize the chatbot service
+    if user_input.lower() == "exit":
+        return "Goodbye!"
+    response = chatbot_service.get_fireworks_response(user_input)
+    return response
+
+# Create the Gradio interface
+iface = gr.Interface(
+    fn=chatbot_response,  # Function to process user input
+    inputs=gr.Textbox(lines=2, placeholder="Type your message here..."),
+    outputs="text",
+    title="Palantíre - Your Tolkien Guide",
+    description=""
+                "Welcome to Palantíre, your AI-powered guide to the world of J.R.R. Tolkien." +
+                "Ask questions about Middle-earth, including The Lord of the Rings, The Hobbit, " +
+                "The Silmarillion, and other related lore. " +
+                "Type your questions below and get answers steeped in the legendarium! " +
+                "Type 'exit' to end the conversation.",
+)
+
+# Launch the interface
+if __name__ == "__main__":
+    iface.launch()
